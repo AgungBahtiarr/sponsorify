87c95594a27b4d76d4ea25f2d2c323d9f9afeb8f
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-12-09 10:46:28 +0700

    feat: add isLogin middleware

---
+++ app/Http/Middleware/isLogin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
+class isLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = Cookie::get("token");
+
+        if ($token == null) {
+            return redirect('/admin/login');
+        } else {
+            return $next($request);
+        }
+    }
+}
