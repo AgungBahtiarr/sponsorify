a2a920876fd70f38c33b84daad7cc44df2155dfb
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-01-15 18:47:41 +0700

    feat: add role seed

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,14 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('roles')->insert([
+            'role' => 'Event Organizer',
+        ]);
+        DB::table('roles')->insert([
+            'role' => 'Sponsorship',
+        ]);
+        DB::table('roles')->insert([
+            'role' => 'Admin',
+        ]);
     }
 }
